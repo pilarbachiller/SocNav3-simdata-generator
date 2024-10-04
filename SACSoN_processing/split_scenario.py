1ccb17eb0c8991c0663becf5ccb1f191094714e0
pilarbachiller <pilar.bachiller@gmail.com>
2024-10-04 18:48:55 +0200

    split scenarios

---
+++ SACSoN_processing/split_scenario.py
@@ -0,0 +1,27 @@
+import json
+import jsbeautifier
+import sys
+import numpy as np
+
+if len(sys.argv)<2:
+    print('Please, specify the name of the JSon file')
+    exit()
+
+file_name = sys.argv[1]
+data = json.load(open(file_name, 'r'))
+
+goal = data['sequence'][0]["goal"]
+robot_trajectories = []
+trajectory = []
+for d in data['sequence']:
+    if d["goal"]["x"] == goal["x"] and d["goal"]["y"] == goal["y"]:
+        trajectory.append([d["robot"]["x"], d["robot"]["y"]])
+    else:
+        robot_trajectories.append(trajectory)
+        goal = d["goal"]
+        trajectory = []
+
+print("number of trajectories", len(robot_trajectories))
+for i in range(len(robot_trajectories)):
+    print("Length trajectory", i, ":", len(robot_trajectories[i]))
+
