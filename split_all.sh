c3bc948fe1e90c436d232ead5a5c451e72707905
Luis J. Manso <luis.manso@gmail.com>
2024-11-03 19:19:26 +0000

    Simplify code

--- split_all.sh
+++ split_all.sh
@@ -1,3 +1,3 @@
 for f in `ls 2024*.json`; do
-	python3 split_multigoal.py $f --videowidth 1400 --videoheight 1400 --leftcrop 180 --rightcrop 430 --topcrop 170 --bottomcrop 520  --rotate 59.5 --ffwd --novideo #2> /dev/null
+	python3 split_multigoal.py $f 
 done
